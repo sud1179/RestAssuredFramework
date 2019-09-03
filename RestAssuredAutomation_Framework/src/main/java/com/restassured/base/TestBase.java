868f282b24ab83f8201143fbfc999b14b2cdc09d
Yugain <yugain@github.com>
2019-09-03 08:50:57 +0530

    New Test Cases

--- RestAssuredAutomation_Framework/src/main/java/com/restassured/base/TestBase.java
+++ RestAssuredAutomation_Framework/src/main/java/com/restassured/base/TestBase.java
@@ -1,11 +1,20 @@
 package com.restassured.base;
 
+import org.testng.annotations.Test;
+
 import io.restassured.RestAssured;
+import io.restassured.http.Method;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 
 public class TestBase {
-
-	RequestSpecification httpRequest=RestAssured.given();
-	Response response;
+   
+		
+		public static RequestSpecification httpRequest;
+		
+		public static Response response;
+		
+	
+	
+	
 }
